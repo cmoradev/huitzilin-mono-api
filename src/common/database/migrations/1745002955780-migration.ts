1727b85045dcd6379c2040cdfc8e168734f37b3f
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-04-21 11:58:35 -0500

    feat(migration): remove unused indexes and drop levels_to_students table
    - Dropped indexes IDX_de6da5b7a2989cef130012c6df and IDX_ecd4dfdd58893b5810d22d9701 from the school schema.
    - Removed the levels_to_students table as part of the migration process.

--- src/common/database/migrations/1745002955780-migration.ts
+++ src/common/database/migrations/1745002955780-migration.ts
@@ -13,6 +13,13 @@ export class Migration1745002955780 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "school"."enrollments" ADD CONSTRAINT "FK_6f7f2c7a6778c2e86e41db3ce0d" FOREIGN KEY ("levelId") REFERENCES "school"."levels"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
+    await queryRunner.query(
+      `DROP INDEX "school"."IDX_de6da5b7a2989cef130012c6df"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "school"."IDX_ecd4dfdd58893b5810d22d9701"`,
+    );
+    await queryRunner.query(`DROP TABLE "school"."levels_to_students"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
